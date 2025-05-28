48195b31be595e3699d15606345a7a104bcea0d8
Mounika <mounikaedara05@gmail.com>
2025-05-28 15:40:12 +0530

    preparing the eda

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 # Logging setup
 logging.basicConfig(
@@ -16,7 +16,7 @@ def error_message_detail(error, error_detail: sys):
     )
     return error_message
 
-class CustomException(Exception):  # ✅ class not def
+class CustomException(Exception):  
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
